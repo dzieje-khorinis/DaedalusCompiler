4a624fffaf5f04ddeb58c6d185d34c00fcd650ab
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-04-14 10:20:23 +0200

    Create mvp-test-1.d

---
+++ test/DaedalusCompiler.Tests/mvp-test-1.d
@@ -0,0 +1,6 @@
+// these 4 symbols are required by original parser
+// since we want to compare results, we have to start with them as most simple input
+func void startup_global() {};
+func void init_global() {};
+class C_Npc { var int data [200]; };
+instance PC_HERO(C_Npc) {};
