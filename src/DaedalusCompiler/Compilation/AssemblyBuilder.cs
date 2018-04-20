7eb8941ef054f6db9ff34507d31bcfc8b68989e6
Artur <arturmiks@gmail.com>
2018-04-20 15:52:04 +0200

    Adjusted builder

--- src/DaedalusCompiler/Compilation/AssemblyBuilder.cs
+++ src/DaedalusCompiler/Compilation/AssemblyBuilder.cs
@@ -7,11 +7,39 @@ namespace DaedalusCompiler.Compilation
 {
     public class AssemblyInstruction
     {
-        public DatToken opcode;
+        public DatTokenType opcode;
+    }
+
+    public class SymbolInstruction : AssemblyInstruction
+    {
         public DatSymbol symbol;
     }
 
-    public class AseemblyFunction
+    public class ValueInstruction : AssemblyInstruction
+    {
+        public object value;
+    }
+
+    public class AddressInstruction : AssemblyFunction
+    {
+        public int address;
+    }
+
+    public class ParamLessInstruction : AssemblyInstruction{}
+
+    public class PushInt : ValueInstruction {}
+    
+    public class PushVar : SymbolInstruction {}
+    
+    public class Less : ParamLessInstruction {}
+    public class Greater : ParamLessInstruction {}
+    public class Assign : ParamLessInstruction {}
+    public class AssignString : ParamLessInstruction {}
+    public class Ret : ParamLessInstruction {}
+    
+    public class JumpIf : AddressInstruction {}
+
+    public class AssemblyFunction
     {
         public List<DatSymbol> parameters;
         public List<AssemblyInstruction> body;
@@ -19,21 +47,26 @@ namespace DaedalusCompiler.Compilation
     
     public class AssemblyBuilder
     {
-        private Dictionary<string, AseemblyFunction> functions;
+        private Dictionary<string, AssemblyFunction> functions;
         private Dictionary<string, DatSymbol> symbols;
         
         public AssemblyBuilder()
         {
-            functions = new Dictionary<string, AseemblyFunction>();
+            functions = new Dictionary<string, AssemblyFunction>();
             symbols = new Dictionary<string, DatSymbol>();
         }
 
-        public void RegisterFunction(String name, AseemblyFunction func)
+        public void RegisterFunction(string name, AssemblyFunction func)
         {
             functions.Add(name, func);
         }
 
-        public void RegisterSymbol(String name, DatSymbol symbol)
+//        public void getParamSymbol()
+//        {
+//            
+//        }
+
+        public void RegisterSymbol(string name, DatSymbol symbol)
         {
             symbols.Add(name, symbol);
         }
