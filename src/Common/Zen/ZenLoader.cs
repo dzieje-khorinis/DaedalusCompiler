1827be8912f467b9e5f5675f22e70d75bceb65fd
Mona Lisa <arturmiks@gmail.com>
2025-08-22 21:57:13 +0200

    Revert "Revert old code"
    This reverts commit 3e23bc8f47f68b19cabef11d093afc609ad954a8.

--- src/Common/Zen/ZenLoader.cs
+++ src/Common/Zen/ZenLoader.cs
@@ -93,7 +93,7 @@ namespace Common.Zen
 
         private IParseTree Parse(string zenContent)
         {
-            string[] fileContentLines = zenContent.Split(Environment.NewLine);
+            string[] fileContentLines = zenContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
             _filesContentsLines.Add(fileContentLines);
 
             AntlrInputStream inputStream = new AntlrInputStream(zenContent);
